82483990cfa8e37aee5469b5d479fe296194d0ea
lorello <lorenzo.salvadorini@softecspa.it>
2015-03-31 01:21:31 +0200

    fixed style

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 class puppet (
-  $version = '',
-  $day_before_renew = '30'
-) {
+  $version          = '',
+  $day_before_renew = '30') {
   # COMMON VARIABLES
   $puppet_run = '/usr/local/sbin/puppet-run'
   $puppet_cert_renew = '/usr/local/sbin/puppet-cert-renew'
@@ -16,50 +15,51 @@ class puppet (
 
   # Module defaults
   File {
-    group   => 'admin',
-    mode    => '0664',
+    group => 'admin',
+    mode  => '0664',
   }
 
   Package {
-    require => [ Class['apt'], Apt::Source['puppetlabs'] ],
+    require => [
+      Class['apt'],
+      Apt::Source['puppetlabs']],
   }
 
   # clean old unused repository
-  apt::source { 'puppet': ensure  => absent, }
-
-
+  apt::source { 'puppet': ensure => absent, }
 
   case $::lsbdistcodename {
     'hardy' : {
       $puppet_env_version = $environment ? {
-        'production'  => '2.7.18-1puppetlabs1',
-        default       => '2.7.20~hardy~ppa1',
+        'production' => '2.7.18-1puppetlabs1',
+        default      => '2.7.20~hardy~ppa1',
       }
       $facter_version = '1.6.18~lucid~ppa1'
     }
 
-    default: {
+    default : {
       $puppet_env_version = '3.7.3-1puppetlabs1'
       $facter_version = '2.3.0-1puppetlabs1'
     }
   }
 
-  apt::source {'puppetlabs':
-    location  => 'http://apt.puppetlabs.com',
-    repos     => 'main',
-    key       => '4BD6EC30',
+  apt::source { 'puppetlabs':
+    location => 'http://apt.puppetlabs.com',
+    repos    => 'main',
+    key      => '4BD6EC30',
   }
-  apt::source {'puppetlabs-deps':
-    location  => 'http://apt.puppetlabs.com',
-    repos     => 'dependencies',
-    key       => '4BD6EC30',
+
+  apt::source { 'puppetlabs-deps':
+    location => 'http://apt.puppetlabs.com',
+    repos    => 'dependencies',
+    key      => '4BD6EC30',
   }
 
   # for latest augeas packages. Not for trusty
   if $::lsbdistcodename != 'trusty' {
-    softec_apt::ppa{'skettler/puppet':
-      key     => 'C18789EA',
-      mirror  => true
+    softec_apt::ppa { 'skettler/puppet':
+      key    => 'C18789EA',
+      mirror => true
     }
   }
 
@@ -70,65 +70,86 @@ class puppet (
 
   if $::lsbdistcodename == 'precise' {
     # aggiungo questo ppa per precise che contiene augeas alla versione 1.0.0
-    #apt::key { 'AE498453': }
-    #apt::sources_list { 'augeas':
-    #  content => "deb http://ppa.launchpad.net/raphink/augeas-1.0.0/ubuntu ${::lsbdistcodename} main\ndeb-src http://ppa.launchpad.net/raphink/augeas-1.0.0/ubuntu ${::lsbdistcodename} main",
+    # apt::key { 'AE498453': }
+    # apt::sources_list { 'augeas':
+    #  content => "deb http://ppa.launchpad.net/raphink/augeas-1.0.0/ubuntu
+    #  ${::lsbdistcodename} main\ndeb-src http://ppa.launchpad.net/raphink/augeas-1.0.0/ubuntu ${::lsbdistcodename} main",
     #}
 
-    softec_apt::ppa {'raphink/augeas-1.0.0':
-      key     => 'AE498453',
-      #mirror  => true
-    }
+    softec_apt::ppa { 'raphink/augeas-1.0.0': key => 'AE498453',
+    # mirror  => true
+     }
 
-    softec_apt::ppa {'brightbox/ruby-ng':
-      key => 'C3173AA6'
+    softec_apt::ppa { 'brightbox/ruby-ng': key => 'C3173AA6' } ->
+    package {
+      'ruby1.9.1':
+        ensure => present;
+
+      'ruby1.9.1-dev':
+        ensure => present;
+
+      'ruby-switch':
+        ensure => present;
     } ->
-
-    package{
-      'ruby1.9.1':      ensure => present;
-      'ruby1.9.1-dev':  ensure => present;
-      'ruby-switch':    ensure => present;
-    } ->
-
-    exec {'switch ruby1.9.1':
+    exec { 'switch ruby1.9.1':
       command => 'ruby-switch --set ruby1.9.1',
       unless  => 'ruby-switch --check | grep Currently | awk \'{print $3}\' | grep \'ruby1.9.1\''
     }
 
   }
 
-  $libaugeas_ruby_v = $::lsbdistcodename? {
+  $libaugeas_ruby_v = $::lsbdistcodename ? {
     'lucid' => 'libaugeas-ruby1.8',
     default => 'libaugeas-ruby1.9.1'
   }
-  $rubygems_package_name  = $::lsbdistcodename? {
-    'trusty'  => 'rubygems1.9.1',
-    default   => 'rubygems',
+  $rubygems_package_name = $::lsbdistcodename ? {
+    'trusty' => 'rubygems1.9.1',
+    default  => 'rubygems',
   }
 
   # INSTALL
   package {
-    'augeas-lenses':        ensure => latest;
-    'augeas-tools':         ensure => latest;
-    'libaugeas-ruby':       ensure => latest;
-    $libaugeas_ruby_v:      ensure => latest;
-    $rubygems_package_name: ensure => latest;
-    'libaugeas0':           ensure => latest;
+    'augeas-lenses':
+      ensure => latest;
+
+    'augeas-tools':
+      ensure => latest;
+
+    'libaugeas-ruby':
+      ensure => latest;
+
+    $libaugeas_ruby_v:
+      ensure => latest;
+
+    $rubygems_package_name:
+      ensure => latest;
+
+    'libaugeas0':
+      ensure => latest;
   }
 
-  apt::pin { 'puppet':
-    packages  => [ 'puppet', 'puppet-common' ],
-    version   => $puppet_version,
-    priority  => '1001';
-  'facter':
-    packages  => 'facter',
-    version   => $facter_version,
-    priority  => '1001'
+  apt::pin {
+    'puppet':
+      packages => [
+        'puppet',
+        'puppet-common'],
+      version  => $puppet_version,
+      priority => '1001';
+
+    'facter':
+      packages => 'facter',
+      version  => $facter_version,
+      priority => '1001'
   } ->
   package {
-    'puppet':               ensure => $puppet_version;
-    'puppet-common':        ensure => $puppet_version;
-    'facter':               ensure => $facter_version;
+    'puppet':
+      ensure => $puppet_version;
+
+    'puppet-common':
+      ensure => $puppet_version;
+
+    'facter':
+      ensure => $facter_version;
   }
 
   # CONFIG
@@ -144,9 +165,9 @@ class puppet (
   }
 
   file { '/var/log/puppet':
-    ensure  => directory,
-    group   => 'adm',
-    mode    => '2770',
+    ensure => directory,
+    group  => 'adm',
+    mode   => '2770',
   }
 
   file { '/etc/default/puppet':
@@ -156,25 +177,10 @@ class puppet (
 
   # #569: disable running as a service
   exec { 'puppet-disable-startup':
-    command     => '/bin/sed \'s/^START=yes/START=no/i\' -i~ /etc/default/puppet',
-    unless      => '/bin/grep -q \'^START=no\' /etc/default/puppet',
-    require     => File['/etc/default/puppet'],
+    command => '/bin/sed \'s/^START=yes/START=no/i\' -i~ /etc/default/puppet',
+    unless  => '/bin/grep -q \'^START=no\' /etc/default/puppet',
+    require => File['/etc/default/puppet'],
   }
-
-  #569: gestione di Puppet tramite cron, dovrebbe aver girato in
-  # abbondanza su tutti i server
-  # stop service if running
-  # exec { "puppet-stop-service":
-  #    command     => "echo /etc/init.d/puppet stop | at now + 15 minutes",
-  #    onlyif      => "ps aux | grep -v grep | grep 'puppet agent$'",
-  #    require     => Exec["puppet-disable-startup"],
-  # }
-
-  # Configurazione agent/master
-  #file { "/etc/puppet/puppet.conf":
-  #    ensure  => present,
-  #    content => template("puppet/etc/puppet-master.conf.erb"),
-  #}
 
   # Script that runs puppet until it exists whitout errors
   file { $puppet_run:
@@ -196,39 +202,38 @@ class puppet (
   file {
     '/etc/puppet/etckeeper-commit-pre':
       source  => 'puppet:///modules/puppet/etc/etckeeper-commit-pre',
-      mode  => '0775',
+      mode    => '0775',
       require => Package['puppet'];
+
     '/etc/puppet/etckeeper-commit-post':
       source  => 'puppet:///modules/puppet/etc/etckeeper-commit-post',
-      mode  => '0775',
+      mode    => '0775',
       require => Package['puppet'];
   }
 
   # da aggiungere??
-  #"set agent/certname $fqdn",
-  #"set agent/server $server",
-  #"set agent/environment ${puppet_env}",
+  # "set agent/certname $fqdn",
+  # "set agent/server $server",
+  # "set agent/environment ${puppet_env}",
   augeas { 'puppet-agent-config':
     context => '/files/etc/puppet/puppet.conf',
     changes => [
-      "set main/prerun_command /etc/puppet/etckeeper-commit-pre",
-      "set main/postrun_command /etc/puppet/etckeeper-commit-post",
-      "set main/pluginsync true", # non serve più da puppet 3.x
-      "set agent/masterport 443",
-      "set agent/pluginsync true", # non serve più da puppet 3.x
-      "set agent/graph true",
-      "set agent/graphdir /var/puppet",
-      "set agent/report true"
-    ],
+      'set main/prerun_command /etc/puppet/etckeeper-commit-pre',
+      'set main/postrun_command /etc/puppet/etckeeper-commit-post',
+      'set main/pluginsync true', # non serve più da puppet 3.x
+      'set agent/masterport 443',
+      'set agent/graph true',
+      'set agent/graphdir /var/puppet',
+      'set agent/report true'],
     require => [
       File['/etc/puppet/etckeeper-commit-pre'],
       File['/etc/puppet/etckeeper-commit-post'],
       Package['puppet'],
-    ],
+      ],
   }
 
   case $environment {
-    'production': {
+    'production' : {
       # in produzione, una volta tra le 11 e le 13 e una volta
       # tra le 14 e le 16, dal lunedi al giovedi compresi
       # Fx - sposto l'intervallo del mattino a 9-13
@@ -242,18 +247,24 @@ class puppet (
       cron { 'puppet-cron-onetime':
         command => $puppet_run_cmd_prod,
         user    => 'root',
-        weekday => [ 1,2,3,4 ],
-        minute  => [ $minute ],
-        hour    => [ $hour1, $hour2, $hour3 ],
+        weekday => [
+          1,
+          2,
+          3,
+          4],
+        minute  => [$minute],
+        hour    => [
+          $hour1,
+          $hour2,
+          $hour3],
         require => [
-          File[ $puppet_run ],
+          File[$puppet_run],
           File['/var/log/puppet'],
-          File['/usr/local/bin/solo']
-          ],
+          File['/usr/local/bin/solo']],
       }
     }
 
-    default: {
+    default      : {
       # tutto ciò che non è produzione è sviluppo
       # una esecuzione ogni ora, tutti i giorni in 8-14
       $minute = fqdn_rand(59)
@@ -262,17 +273,18 @@ class puppet (
         command => $puppet_run_cmd_dev,
         user    => 'root',
         weekday => '*',
-        minute  => [ $minute ],
+        minute  => [$minute],
         hour    => '8-14',
         require => [
-          File[ $puppet_run ],
+          File[$puppet_run],
           File['/var/log/puppet'],
-          File['/usr/local/bin/solo'] ],
+          File['/usr/local/bin/solo']],
       }
     }
   }
 
   $minute_renew = fqdn_rand(59)
+
   # rinnovo dei certificati
   cron { 'puppet-cert-renew':
     command => '/usr/local/sbin/puppet-cert-renew > /dev/null',
