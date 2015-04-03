20a1bab0b17264aa9dff50aae974a05968329678
lorello <lorenzo.salvadorini@softecspa.it>
2015-04-03 14:08:29 +0200

    enabled always_cache_features on master for a little more performance

--- manifests/master.pp
+++ manifests/master.pp
@@ -176,6 +176,7 @@ class puppet::master (
       "set master/reports ${reports}",
       'set master/ssl_client_header SSL_CLIENT_S_DN',
       'set master/ssl_client_verify_header SSL_CLIENT_VERIFY',
+      'set master/always_cache_features true',
       ]
   }
 
